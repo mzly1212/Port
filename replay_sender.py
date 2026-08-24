57818eb774a5cc9b4480920bccf3f791a0769fe5
mzly1212 <2876059771@qq.com>
2026-08-24 16:39:28 +0800

    克隆 修改 提交 测试

--- replay_sender.py
+++ replay_sender.py
@@ -11,6 +11,9 @@ from config import Config
 from data import ProcessedVehicle, ProcessedFrame
 from output_adapter import ProtocolAdapter
 
+"""
+克隆 修改 提交 测试
+"""
 
 class ReplaySender:
     def __init__(self, mode: str = "fixed", speed: float = 1.0, loop: bool = False,

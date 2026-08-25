627609d2e61e9f24f56fa7cd4beb744dce7c2d2e
mzly1212 <2876059771@qq.com>
2026-08-25 10:18:56 +0800

    星联协议解析文件(误动)

--- pb2.py
+++ pb2.py
@@ -14,8 +14,6 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 
 
-
-
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='nebulalink.perceptron3.0.5.proto',
   package='nebulalink.perceptron',
